c9b61aef4110250eaefff8d3b0881f1f5e4cabf8
CHETTI SAI SRINIVAS <srinu.samm@gmail.com>
2017-02-06 15:09:00 +0530

    Add files via upload

---
+++ prob67.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class prob67 {
+    public static void main(String[] args) throws IOException{
+        String file = "D:\\Eclipse\\p067_triangle.txt";
+        Scanner s = new Scanner(new File(file));
+        int lines=0;
+        while(s.hasNextLine()) {
+            lines++;
+            s.nextLine();
+        }
+        System.out.println("Lines:"+lines);
+        int[][] dp = new int[lines][lines];
+        Scanner sc = new Scanner(new File(file));
+        for(int i=0;i<lines;i++){
+            int j=0;
+            while(j<i+1){
+                if(sc.hasNextInt()) dp[i][j] = sc.nextInt();
+                //System.out.println(dp[i][j]);
+                j++;
+            }
+        }
+        for(int i=lines-1;i>0;i--){
+            int j=0;
+            while(j<i){
+                dp[i-1][j]+=Math.max(dp[i][j],dp[i][j+1]);
+                j++;
+            }
+        }
+        System.out.println("the maximum total from top to bottom of the triangle is " +dp[0][0]);
+    }
+}
